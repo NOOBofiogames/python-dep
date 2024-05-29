da082dbbc43089eb4eeb2301315742fb0d96a325
NOOBofiogames <163781843+NOOBofiogames@users.noreply.github.com>
2024-05-29 19:00:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-# Importing the relevant libraries
-import websockets
+#!/usr/bin/env python
+
 import asyncio
+import http
+import signal
 
-PORT = 25562
+import websockets
 
-print("Server listening on Port " + str(PORT))
 
-async def echo(websocket, path):
-    print("A client just connected")
-    try:
-        async for message in websocket:
-            print("Received message from client: " + message)
-            await websocket.send("Pong: " + message)
-    except websockets.exceptions.ConnectionClosed as e:
-        print("A client just disconnected")
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
 
-start_server = websockets.serve(echo, "localhost", PORT)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+async def health_check(path, request_headers):
+    if path == "/healthz":
+        return http.HTTPStatus.OK, [], b"OK\n"
+
+
+async def main():
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    async with websockets.serve(
+        echo,
+        host="",
+        port=8080,
+        process_request=health_check,
+    ):
+        await stop
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
